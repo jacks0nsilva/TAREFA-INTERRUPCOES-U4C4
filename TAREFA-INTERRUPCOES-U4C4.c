281c5b8a06beff95a809a5bc074f8d2d245a1066
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-01-30 20:58:01 -0300

    Definição do pino GPIO do LED vermelho, criada a função blinkLED

--- TAREFA-INTERRUPCOES-U4C4.c
+++ TAREFA-INTERRUPCOES-U4C4.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_PIN 13 // Define o pino GPIO do LED vermelho
 
+void blinkLED(uint duracao){
+    gpio_put(LED_PIN, 1); // Liga o LED
+    sleep_ms(duracao); // Aguarda a duração
+    gpio_put(LED_PIN, 0); // Desliga o LED
+    sleep_ms(duracao); // Aguarda a duração
+}
 
 int main()
 {
     stdio_init_all();
-
+    gpio_init(LED_PIN); // Inicializa o pino GPIO do LED vermelho
+    gpio_set_dir(LED_PIN, GPIO_OUT); // Define o pino GPIO do LED como saída
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        blinkLED(100); // LED vermelho pisca continuamente 5 vezes por segundo
     }
 }
